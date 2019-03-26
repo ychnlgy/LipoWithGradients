0ac5929ea70b9052357de724d49ddceddd2e67f0
Yuchen Li <ychnlgy@gmail.com>
2019-03-26 17:42:54 -0400

    Added code

---
+++ src/NeuralGlobalOptimizer.py
@@ -0,0 +1,160 @@
+import torch, math
+
+from GlobalOptimizer import *
+
+class NeuralGlobalOptimizer(GlobalOptimizer):
+
+    EPS = 1e-32
+    SELECTION = 0.5
+
+    def __init__(self, *args, prep_visualization=False, **kwargs):
+        self.network_retrain_count = 0
+
+        self.prep_visualization = prep_visualization
+        if prep_visualization:
+            self.data_losses = []
+            self.test_losses = []
+            self.feature_counts = []
+            self.store_losses = self._store_losses
+
+        super().__init__(*args, **kwargs)
+
+    def count_network_retrains(self):
+        return self.network_retrain_count
+
+    def get_losses(self):
+        assert self.prep_visualization
+        return (self.data_losses, self.test_losses, self.feature_counts)
+
+    # === PRIVATE ===
+
+    def get_dataset(self):
+        raise NotImplementedError
+
+    def make_model(self, D):
+        raise NotImplementedError
+
+    def train_model(self, model, lossf, X, Y):
+        raise NotImplementedError
+
+    def create_model_lossfunction(self):
+        raise NotImplementedError
+
+    def penalize_featurecount(self, count):
+        raise NotImplementedError
+
+    def create_evalnet(self, D):
+        raise NotImplementedError
+
+    def train_evalnet(self, evalnet, X, Y):
+        raise NotImplementedError
+
+    def store_losses(self, *args):
+        return
+
+    def _store_losses(self, data_loss, test_loss, feature_count):
+        self.data_losses.append(data_loss)
+        self.test_losses.append(test_loss)
+        self.feature_counts.append(feature_count)
+
+    def create_optimizer(self, parameters, lr):
+        '''
+
+        Output:
+            optim - torch.optim.Optimizer for the input parameters.
+
+        '''
+        return torch.optim.SGD(parameters, lr=lr)
+
+    def evaluate(self, x):
+        '''
+
+        Input:
+            x - torch Tensor of shape (D), feature selection mask.
+
+        Output:
+            y - torch Tensor of shape (1), scores for using x as feature mask.
+
+        '''
+        shortcut = self.lookup_result(x)
+
+        if shortcut is not None:
+            return shortcut
+        else:
+            self.network_retrain_count += 1
+            return self.do_expensive_model_eval(x)
+
+    def lookup_result(self, x):
+        '''
+
+        Output:
+            shortcut - float if x has already been evaluated, otherwise None.
+            
+        '''
+        X, Y = self.get_XY()
+        diff = (x > NeuralGlobalOptimizer.SELECTION) != (X > NeuralGlobalOptimizer.SELECTION)
+
+        # if there is an entry in which no element is
+        # different, then we can use that result.
+        mark = diff.long().sum(dim=1) == 0
+        if mark.long().sum() > 0:
+            shortcut = Y[mark]
+            assert len(shortcut) == 1
+            return shortcut.item()
+        else:
+            return None
+        
+    def do_expensive_model_eval(self, x):
+        X_data, Y_data, X_test, Y_test = self.get_dataset()
+        X_data *= x.unsqueeze(0)
+        model = self.make_model(X_data.size(1))
+        lossf = self.create_model_lossfunction()
+        model.train()
+        self.train_model(model, lossf, X_data, Y_data)
+
+        with torch.no_grad():
+            model.eval()
+            Yh_data = model(X_data)
+            data_loss = torch.log(
+                lossf(Yh_data, Y_data) + NeuralGlobalOptimizer.EPS
+            ).item()
+            Yh_test = model(X_test)
+            test_loss = torch.log(
+                lossf(Yh_test, Y_test) + NeuralGlobalOptimizer.EPS
+            ).item()
+            feature_count = x.sum().item()
+            feature_penalty = self.penalize_featurecount(feature_count)
+            self.store_losses(data_loss, test_loss, feature_count)
+            return -(data_loss + test_loss + feature_penalty)
+
+    def fit_evalnet(self, X, Y):
+        '''
+
+        Input:
+            X - torch Tensor of shape (N, D), feature selection masks.
+            Y - torch Tensor of shape (N), scores for each selection.
+
+        Output:
+            evalnet - torch.nn.Module, Lipschitz network that maps
+                feature selection to predicted score.
+
+        '''
+        self._used_gradpenalty = False
+        evalnet = self.create_evalnet(X.size(1))
+        evalnet.train()
+        self.train_evalnet(evalnet, X, Y)
+        assert self._used_gradpenalty
+        evalnet.eval()
+        return evalnet
+
+    def grad_penalty(self, evalnet, X):
+        self._used_gradpenalty = True
+        X = torch.autograd.Variable(X, requires_grad=True)
+        Y = evalnet(X).sum()
+        T = list(evalnet.parameters()) + [X]
+        grads = torch.autograd.grad([Y], T, create_graph=True)
+        return sum(map(NeuralGlobalOptimizer.lipschitz1_loss, grads))
+
+    @staticmethod
+    def lipschitz1_loss(g):
+        return torch.nn.functional.relu(g.abs()-1).sum()
---
+++ src/equal4.py
@@ -0,0 +1,126 @@
+import torch, tqdm, math
+import torch.utils.data
+
+from NeuralGlobalOptimizer import *
+from MovingAverage import MovingAverage
+
+class Equal4(NeuralGlobalOptimizer):
+
+    D = 100
+
+    def get_dataset(self):
+        N = 500
+        true_D = 4
+        X = torch.rand(N*2, Equal4.D)
+        Y = X[:,:true_D].sum(dim=1)
+        return X[:N], Y[:N], X[N:], Y[N:]
+
+    def make_model(self, D):
+        return torch.nn.Linear(D, 1)
+
+    def train_model(self, model, lossf, X, Y):
+        dataset = torch.utils.data.TensorDataset(X, Y)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True)
+
+        epochs = 100
+
+        optim = torch.optim.Adam(model.parameters())
+        sched = torch.optim.lr_scheduler.MultiStepLR(optim, milestones=[50])
+
+        for epoch in range(epochs):
+
+            for x, y in dataloader:
+                yh = model(x).squeeze()
+                loss = lossf(yh, y)
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+
+            sched.step()
+
+    def create_model_lossfunction(self):
+        return torch.nn.MSELoss()
+
+    def penalize_featurecount(self, count):
+        return 1e-4 * count**2
+
+    def create_evalnet(self, D):
+        return torch.nn.Sequential(
+            torch.nn.Linear(D, 64),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(64, 64),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(64, 1)
+        )
+
+    def train_evalnet(self, evalnet, X, Y):
+        epochs = 300
+        lossf = torch.nn.MSELoss()
+        optim = torch.optim.Adam(evalnet.parameters())
+        sched = torch.optim.lr_scheduler.MultiStepLR(optim, milestones=[100, 200])
+
+        bar = tqdm.tqdm(range(epochs), ncols=80)
+        avg = MovingAverage(momentum=0.95)
+        
+        for epoch in bar:
+            Yh = evalnet(X)
+            loss = lossf(Yh, Y) + self.grad_penalty(evalnet, X)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            sched.step()
+            avg.update(loss.item())
+            bar.set_description("Fitting evalnet: %.3f" % avg.peek())
+
+    @staticmethod
+    def main():
+        features = Equal4.D
+        prog = Equal4(
+            init_X = torch.rand(8, features),
+            explore = 8,
+            exploit = 2,
+            table = GlobalOptimizationTable(
+                capacity = 4000,
+                features = features,
+                reduced_size = 3000,
+                montecarlo_c = math.sqrt(2)
+            ),
+            lipo = Lipo(k=1, d=features, a=0, b=1),
+            max_retry = 10,
+            lr = 0.1,
+            savepath = "equal4.pkl",
+            prep_visualization = True
+        )
+
+        for i in range(2):
+            print(" === Epoch %d ===" % i)
+            prog.step()
+
+        X, Y = prog.publish_XY()
+
+        best_n = 10
+        x = (X[:best_n] > NeuralGlobalOptimizer.SELECTION).numpy()
+        print(" === Top %d feature selections === " % best_n, x, sep="\n")
+        print(" >>> Number of retraining operations: %d" % prog.count_network_retrains())
+        
+        data_loss, test_loss, feature_counts = prog.get_losses()
+
+        from matplotlib import pyplot
+        fig, axes = pyplot.subplots(nrows=3, sharex=True, figsize=(10, 8))
+
+        axes[0].plot(data_loss)
+        axes[0].set_ylabel("Training loss")
+
+        axes[1].plot(test_loss)
+        axes[1].set_ylabel("Validation loss")
+
+        axes[2].plot(feature_counts)
+        axes[2].set_ylabel("Feature count")
+
+        axes[-1].set_xlabel("Evaluations")
+
+        pyplot.savefig("equal4.png")
+
+if __name__ == "__main__":
+
+    Equal4.main()
