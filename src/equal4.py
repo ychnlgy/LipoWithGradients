7c6974a9d1d5becb9df9f2655d3f3dcc3bc39f69
Yuchen Li <ychnlgy@gmail.com>
2019-03-27 16:40:38 -0400

    Uploaded code

--- src/equal4.py
+++ src/equal4.py
@@ -47,7 +47,7 @@ class Equal4(NeuralGlobalOptimizer):
         return torch.nn.MSELoss()
 
     def penalize_featurecount(self, count):
-        return 1e-4 * count
+        return 1e-3 * count
 
     def create_evalnet(self, D):
         '''
