bbfc87e0af32637d16e5003cb7034a4015edafe1
Yuchen Li <ychnlgy@gmail.com>
2019-03-27 15:19:38 -0400

    Uploaded code

--- src/NeuralGlobalOptimizer.py
+++ src/NeuralGlobalOptimizer.py
@@ -97,6 +97,7 @@ class NeuralGlobalOptimizer(GlobalOptimizer):
             shortcut - float if x has already been evaluated, otherwise None.
             
         '''
+        return None
         if not len(self.table):
             return None
         
