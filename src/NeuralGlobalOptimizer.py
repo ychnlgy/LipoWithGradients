33774d64bcf62561dc6f4b060bcc758c4c1ab3d5
Yuchen Li <ychnlgy@gmail.com>
2019-03-28 18:10:50 -0400

    Upload code

--- src/NeuralGlobalOptimizer.py
+++ src/NeuralGlobalOptimizer.py
@@ -149,6 +149,7 @@ class NeuralGlobalOptimizer(GlobalOptimizer):
             # WARNING (previous bug): D is reduced
             feature_penalty = self.penalize_featurecount(feature_count, len(x))
             self.store_losses(data_loss, test_loss, feature_count)
+            print("\n", data_loss, test_loss)
             return -(data_loss + test_loss + feature_penalty)
 
     @staticmethod
