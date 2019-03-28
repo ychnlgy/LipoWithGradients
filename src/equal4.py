7c36501229840c769ae989e99038c204972eb4f2
Yuchen Li <ychnlgy@gmail.com>
2019-03-28 15:59:26 -0400

    Upload code

--- src/equal4.py
+++ src/equal4.py
@@ -132,10 +132,10 @@ def main(cycles, features):
     Equal4.D = features
     prog = Equal4(
         features = features,
-        gradpenalty_weight = 1e-3,
+        gradpenalty_weight = 1e-2,
         explore = 8,
         exploit = 8,
-        mutation_rate = 0.01,
+        mutation_rate = 0.05,
         expected_train_loss = 0.01,
         featurepenalty_frac = 10,
         table = GlobalOptimizationTable(
