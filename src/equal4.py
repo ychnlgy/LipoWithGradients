d026367c741759ea7aafa71c66d6ff8ea1d30e16
Yuchen Li <ychnlgy@gmail.com>
2019-03-29 17:08:01 -0400

    Upload code

--- src/equal4.py
+++ src/equal4.py
@@ -146,6 +146,7 @@ class Equal4(NeuralGlobalOptimizer):
         I = (Y - Y.max()).abs() < 1e-4
         Y_max = Y[I]
         X_max = X[I]
+        print(X_max, Y_max)
         
         for epoch in bar:
             for Xb, Yb in dataloader:
