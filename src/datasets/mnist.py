5ebac6570f32072b82be30da8b998f57dd528930
Yuchen Li <ychnlgy@gmail.com>
2019-04-14 15:32:18 -0400

    Uploaded MNIST code

--- src/datasets/mnist.py
+++ src/datasets/mnist.py
@@ -13,14 +13,18 @@ def get(download=0):
     CHANNELS = 1
     IMAGESIZE = (32, 32)
     
-    train = torchvision.datasets.MNIST(root=constants.DATA_ROOT, train=True, download=download)
-    data_X = train.train_data.view(-1, 1, 28, 28).float()/255.0
-    data_X = util.convert_size(data_X, IMAGESIZE)
+    train = torchvision.datasets.MNIST(
+        root=constants.DATA_ROOT, 
+        train=True, download=download, 
+        transform=torchvision.transforms.RandomCrop((32, 32), padding=4)
+    )
+    data_X = train.train_data.view(-1, 1, 32, 32).float()/255.0
+    #data_X = util.convert_size(data_X, IMAGESIZE)
     data_Y = torch.LongTensor(train.train_labels)
     
     test = torchvision.datasets.MNIST(root=constants.DATA_ROOT, train=False, download=download)
-    test_X = test.test_data.view(-1, 1, 28, 28).float()/255.0
-    test_X = util.convert_size(test_X, IMAGESIZE)
+    test_X = test.test_data.view(-1, 1, 32, 32).float()/255.0
+    #test_X = util.convert_size(test_X, IMAGESIZE)
     test_Y = torch.LongTensor(test.test_labels)
 
     return data_X, data_Y, test_X, test_Y, CLASSES, CHANNELS, IMAGESIZE
