0add7732b10fa1cab089d1163670a9f5c915a818
Yuchen Li <ychnlgy@gmail.com>
2019-04-14 14:53:48 -0400

    Uploaded MNIST code

--- src/mnist-polynomial.py
+++ src/mnist-polynomial.py
@@ -10,9 +10,9 @@ def create_baseline_model(D, C):
         modules.ResNet(
             modules.ResBlock(
                 block = torch.nn.Sequential(
-                    #torch.nn.BatchNorm2d(32),
-                    #torch.nn.ReLU(),
-                    #torch.nn.Conv2d(32, 32, 3, padding=1),
+                    torch.nn.BatchNorm2d(32),
+                    torch.nn.ReLU(),
+                    torch.nn.Conv2d(32, 32, 3, padding=1),
                     
                     torch.nn.BatchNorm2d(32),
                     torch.nn.ReLU(),
@@ -22,9 +22,9 @@ def create_baseline_model(D, C):
             ),
             modules.ResBlock(
                 block = torch.nn.Sequential(
-                    #torch.nn.BatchNorm2d(64),
-                    #torch.nn.ReLU(),
-                    #torch.nn.Conv2d(64, 64, 3, padding=1),
+                    torch.nn.BatchNorm2d(64),
+                    torch.nn.ReLU(),
+                    torch.nn.Conv2d(64, 64, 3, padding=1),
                     
                     torch.nn.BatchNorm2d(64),
                     torch.nn.ReLU(),
@@ -38,8 +38,12 @@ def create_baseline_model(D, C):
                     #torch.nn.ReLU(),
                     #torch.nn.Conv2d(128, 128, 3, padding=1),
                     
-                    torch.nn.BatchNorm2d(128),
-                    torch.nn.ReLU(),
+                    #torch.nn.BatchNorm2d(128),
+                    #torch.nn.ReLU(),
+                    modules.Transpose(1, 3),
+                    modules.PrototypeClassifier(128, 32),
+                    modules.polynomial.Activation(32, n_degree=4),
+                    modules.Transpose(3, 1),
                     torch.nn.Conv2d(128, 256, 3, padding=1, stride=2) # 8 -> 4
                 ),
                 shortcut = torch.nn.Conv2d(128, 256, 1, stride=2)
@@ -48,11 +52,11 @@ def create_baseline_model(D, C):
         torch.nn.AvgPool2d(4),
         modules.Reshape(256),
         torch.nn.Linear(256, 1024),
-        torch.nn.ReLU(),
-        torch.nn.Linear(1024, C)
-        #modules.PrototypeClassifier(1024, 32),
-        #modules.polynomial.Activation(32, n_degree=4),
-        #torch.nn.Linear(32, C)
+        #torch.nn.ReLU(),
+        #torch.nn.Linear(1024, C)
+        modules.PrototypeClassifier(1024, 32),
+        modules.polynomial.Activation(32, n_degree=4),
+        torch.nn.Linear(32, C)
     )
 
 #def create_baseline2(D, C):
