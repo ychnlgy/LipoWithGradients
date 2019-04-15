a7b34588dc88e32e63de0a2d395e61576fcbe7bf
Yuchen Li <ychnlgy@gmail.com>
2019-04-15 14:45:41 -0400

    Added code

--- src/mnist-polynomial.py
+++ src/mnist-polynomial.py
@@ -21,40 +21,40 @@ def create_baseline_model(D, C):
         src.modules.ResNet(
             src.modules.ResBlock(
                 block = torch.nn.Sequential(
-                    torch.nn.BatchNorm2d(32),
-                    #src.modules.PrototypeSimilarity(32, 16),
-                    #src.modules.polynomial.Activation(16, n_degree=4),
-                    torch.nn.ReLU(),
+                    #torch.nn.BatchNorm2d(32),
+                    src.modules.PrototypeSimilarity(32, 32),
+                    src.modules.polynomial.Activation(32, n_degree=16),
+                    #torch.nn.ReLU(),
                     torch.nn.Conv2d(32, 64, 3, padding=1, stride=2) # 32 -> 16
                 ),
                 shortcut = torch.nn.Conv2d(32, 64, 1, stride=2)
             ),
             src.modules.ResBlock(
                 block = torch.nn.Sequential(
-                    torch.nn.BatchNorm2d(64),
-                    #src.modules.PrototypeSimilarity(64, 32),
-                    #src.modules.polynomial.Activation(32, n_degree=4),
-                    torch.nn.ReLU(),
+                    #torch.nn.BatchNorm2d(64),
+                    src.modules.PrototypeSimilarity(64, 64),
+                    src.modules.polynomial.Activation(64, n_degree=16),
+                    #torch.nn.ReLU(),
                     torch.nn.Conv2d(64, 128, 3, padding=1, stride=2) # 16 -> 8
                 ),
                 shortcut = torch.nn.Conv2d(64, 128, 1, stride=2)
             ),
             src.modules.ResBlock(
                 block = torch.nn.Sequential(
-                    torch.nn.BatchNorm2d(128),
-                    src.modules.PrototypeSimilarity(128, 32),
-                    src.modules.polynomial.Activation(32, n_degree=4),
-                    torch.nn.Conv2d(32, 64, 3, padding=1, stride=2) # 8 -> 4
+                    #torch.nn.BatchNorm2d(128),
+                    src.modules.PrototypeSimilarity(128, 128),
+                    src.modules.polynomial.Activation(128, n_degree=16),
+                    torch.nn.Conv2d(128, 256, 3, padding=1, stride=2) # 8 -> 4
                 ),
-                shortcut = torch.nn.Conv2d(128, 64, 1, stride=2)
+                shortcut = torch.nn.Conv2d(128, 256, 1, stride=2)
             )
         ),
         torch.nn.AvgPool2d(4),
-        src.modules.Reshape(64),
-        torch.nn.Linear(64, 256),
-        src.modules.PrototypeSimilarity(256, 64),
-        src.modules.polynomial.Activation(64, n_degree=4),
-        torch.nn.Linear(64, C)
+        src.modules.Reshape(256),
+        torch.nn.Linear(256, 1024),
+        src.modules.PrototypeSimilarity(1024, 128),
+        src.modules.polynomial.Activation(128, n_degree=4),
+        torch.nn.Linear(128, C)
     )
 
 @src.util.main
