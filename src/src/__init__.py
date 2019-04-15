47e0dd2a6d5a7454c3f0e58b9f0c3b379fa947cc
Yuchen Li <ychnlgy@gmail.com>
2019-04-15 13:28:05 -0400

    Added code

--- src/src/__init__.py
+++ src/src/__init__.py
@@ -1,1 +1,1 @@
-from . import algorithm, modules, util, tensortools, cross_validation, winterlightlabs, globaloptim, toy
+from . import algorithm, modules, util, tensortools, winterlightlabs, globaloptim, toy
--- src/src/cross_validation/__init__.py
+++
@@ -1,2 +0,0 @@
-from .leave_one_out import leave_one_out
-from .k_fold import k_fold
