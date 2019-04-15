737073b797b408cbdd2866fe4e9af1ec8c3aa87a
Yuchen Li <ychnlgy@gmail.com>
2019-04-15 18:21:58 -0400

    Added code

--- src/mnist-polynomial.py
+++ src/mnist-polynomial.py
@@ -68,9 +68,11 @@ def create_baseline_model(D, C):
         #torch.nn.Dropout(p=0.2),
         #torch.nn.ReLU(),
         #torch.nn.Linear(256, C)
-        src.modules.PrototypeSimilarity(256, 64),
-        src.modules.polynomial.Activation(64, n_degree=8),
-        torch.nn.Linear(64, C)
+        #src.modules.PrototypeSimilarity(256, 64),
+        #src.modules.polynomial.Activation(64, n_degree=8),
+        torch.nn.Dropout(p=0.2),
+        torch.nn.ReLU(),
+        torch.nn.Linear(256, C)
     )
 
 @src.util.main
