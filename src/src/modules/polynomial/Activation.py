aa110b3db75874b5330acee0d778970b20e49e12
Yuchen Li <ychnlgy@gmail.com>
2019-04-22 15:02:07 -0400

    Added code

--- src/src/modules/polynomial/Activation.py
+++ src/src/modules/polynomial/Activation.py
@@ -2,6 +2,9 @@ import torch, math
 
 from . import chebyshev, LagrangeBasis
 
+import matplotlib
+matplotlib.use("agg")
+
 class Activation(torch.nn.Module):
 
     def __init__(self, input_size, n_degree):
@@ -9,25 +12,36 @@ class Activation(torch.nn.Module):
         self.d = input_size
         self.n = n_degree + 1
         self.basis = LagrangeBasis.create(
-            chebyshev.get_nodes(self.n)
+            chebyshev.get_nodes(n_degree+1)
         )
         self.weight = torch.nn.Parameter(
-            torch.zeros(1, self.d, self.n, 1)
+            torch.zeros(1, self.d, self.n)
         )
+        print(self.visualize)
 
     def forward(self, X):
-        '''
-
-        Input:
-            X - torch Tensor of shape (N, D, *), input features.
-
-        Output:
-            X' - torch Tensor of shape (N, D, *), outputs.
-
-        '''
-        N = X.size(0)
-        D = X.size(1)
-        
-        B = self.basis(X.view(N, D, -1)) # (N, D, n, -1)
-        L = (self.weight * B).sum(dim=2)
+        B = self.basis(X)
+        B = B.view(-1, self.d, self.n)
+        L = (self.weight * B).sum(dim=-1)
         return L.view(X.size())
+
+    def reset_parameters(self):
+        self.weight.data.zero_()
+
+    def visualize(self, k, fname):
+        with torch.no_grad():
+            fig, axes = matplotlib.pyplot.subplots(nrows=k, ncols=1, sharex=True, sharey=True)
+            n = 1000
+            for i in range(k):
+                v = torch.linspace(-1, 1, n)
+                X = torch.zeros(n, self.d)
+                X[:,i] = v
+                Xh = self.forward(X)
+
+                plot = axes[i]
+                plot.plot(v.cpu().numpy(), Xh[:,i].cpu().numpy())
+            
+                self.basis.visualize(plot)
+
+        matplotlib.pyplot.savefig(fname)
+        print("Saved polynomial activations to %s" % fname)
--- src/src/modules/polynomial/LagrangeBasis.py
+++ src/src/modules/polynomial/LagrangeBasis.py
@@ -3,91 +3,46 @@ import torch
 class LagrangeBasis(torch.nn.Module):
 
     @staticmethod
-    def create(nodes, eps=1e-8):
-        '''
-
-        Description:
-            Entry point for instantiating the LagrangeBasis instance.
-            We wish to create the polynomial function based on the
-            coordinates of the input nodes.
-
-        Input:
-            nodes - torch Tensor of shape (n), the x-coordinates of
-                the positions we wish to fit an polynomial.
-            eps - float, for numerical stability of the denominator.
-
-        Output:
-            p - LagrangeBasis, the n-basis functions that will create
-                a polynomial that intersects each x-coordinate exactly.
-
-        '''
+    def create(nodes):
         n = len(nodes)
 
         m_i = (1-torch.eye(n)).byte()
-        m_v = torch.arange(n).long().view(1, n).repeat(n, 1)
+        m_v = torch.arange(n).view(1, n).repeat(n, 1).long()
         m_s = m_v[m_i].view(n, n-1)
 
-        # xm is shape (1, 1, n, n-1, 1)
-        #   - index[2]: number of basis functions.
-        #   - index[3]: points involved per basis function.
-        xm = nodes[m_s].unsqueeze(0).unsqueeze(0).unsqueeze(-1)
-        xj = nodes.view(1, n, 1, 1)
+        xm = nodes[m_s].unsqueeze(0)
+        xj = nodes.view(1, n, 1)
 
-        # dn is shape (1, 1, n, 1)
-        denominator = (xj-xm).prod(dim=3) + eps
-        return LagrangeBasis(xm, denominator)
+        denominator = (xj-xm).prod(dim=2)
+        return LagrangeBasis(xm, denominator, nodes)
+
+    def __init__(self, xm, denominator, nodes):
+        super().__init__()
+        self.nodes = nodes
+        self.register_buffer("xm", xm)
+        self.xm.requires_grad = False
+        self.register_buffer("dn", denominator)
+        self.dn.requires_grad = False
 
     def forward(self, X):
 
         '''
 
         Input:
-            X - torch Tensor of size (N, D, *)
+            X - torch Tensor of size (N, *, D)
 
         Output:
-            L - torch Tensor of size (N, D, n, *), where n is the
+            L - torch Tensor of size (N, *, D, n), where n is the
                 number of nodes for the Lagrange basis.
 
         '''
-        N = X.size(0)
-        D = X.size(1)
-        shape = X.shape[2:] # for restoring size at the end
+
         
-        X = X.view(N, D, 1, 1, -1)
-        out = (X - self.xm).prod(dim=3)/self.dn
-        return out.view(N, D, self.n, *shape)
+        shape = X.size()
+        X = X.view(-1, 1, 1)
+        out = (X-self.xm).prod(dim=-1)/self.dn
+        return out.view(*shape, out.size(-1))
 
-    # === PROTECTED ===
-
-    def __init__(self, xm, denominator):
-        super().__init__()
-        self.register_buffer("xm", xm)
-        self.xm.requires_grad = False
-        self.register_buffer("dn", denominator)
-        self.dn.requires_grad = False
-        self.n = self.dn.size(2)
-
-if __name__ == "__main__":
-
-    def equals_float(v, w):
-        eps = 1e-6
-        return (v-w).norm() < eps
-
-    # Test: {x1 = -1, x2 = 0, x3 = 1}
-    nodes = torch.Tensor([-1, 0, 1])
-    basis = LagrangeBasis.create(nodes)
-
-    # We expect:
-    #   l1(x) = x(x-1)/2
-    #   l2(x) = (x+1)(x-1)/-1
-    #   l3(x) = (x+1)x/2
-
-    assert equals_float(basis.xm, torch.FloatTensor([
-        [ 0, 1],
-        [-1, 1],
-        [-1, 0]
-    ]).view(1, 1, 3, 2, 1)) # xm is what we subtract
-
-    assert equals_float(basis.dn, torch.FloatTensor([
-        [2, -1, 2]
-    ]).view(1, 1, 3, 1)) # dn is what we divide
+    def visualize(self, plot):
+        pass
+        
