41e75d8d81a5ec6aebee68254bff480b7100bf21
Yuchen Li <ychnlgy@gmail.com>
2019-04-22 20:23:56 -0400

    Added code

--- src/mnist-polynomial.py
+++ src/mnist-polynomial.py
@@ -23,13 +23,13 @@ def create_baseline_model(D, C):
         src.modules.ResNet(
             src.modules.ResBlock(
                 block = torch.nn.Sequential(
-                    torch.nn.BatchNorm2d(32),
+                    #torch.nn.BatchNorm2d(32),
                     torch.nn.ReLU(),
                     #src.modules.PrototypeSimilarity(32, 32),
                     #src.modules.polynomial.Activation(32, n_degree=3),
                     torch.nn.Conv2d(32, 32, 3, padding=1),
                     
-                    torch.nn.BatchNorm2d(32),
+                    #torch.nn.BatchNorm2d(32),
                     torch.nn.ReLU(),
                     #src.modules.PrototypeSimilarity(32, 32),
                     #src.modules.polynomial.Activation(32, n_degree=3),
@@ -42,13 +42,13 @@ def create_baseline_model(D, C):
             ),
             src.modules.ResBlock(
                 block = torch.nn.Sequential(
-                    torch.nn.BatchNorm2d(64),
+                    #torch.nn.BatchNorm2d(64),
                     torch.nn.ReLU(),
                     #src.modules.PrototypeSimilarity(64, 64),
                     #src.modules.polynomial.Activation(64, n_degree=3),
                     torch.nn.Conv2d(64, 64, 3, padding=1),
                     
-                    torch.nn.BatchNorm2d(64),
+                    #torch.nn.BatchNorm2d(64),
                     torch.nn.ReLU(),
                     #src.modules.PrototypeSimilarity(64, 32),
                     #src.modules.polynomial.Activation(32, n_degree=3),
@@ -62,7 +62,7 @@ def create_baseline_model(D, C):
             ),
             src.modules.ResBlock(
                 block = torch.nn.Sequential(
-                    torch.nn.BatchNorm2d(128),
+                    #torch.nn.BatchNorm2d(128),
                     torch.nn.ReLU(),
                     #src.modules.PrototypeSimilarity(128, 128),
                     #src.modules.polynomial.Activation(128, n_degree=16),
@@ -71,7 +71,7 @@ def create_baseline_model(D, C):
                     #src.modules.polynomial.Activation(64, n_degree=4),
                     #torch.nn.Conv2d(64, 128, 3, padding=1),
                     
-                    torch.nn.BatchNorm2d(128),
+                    #torch.nn.BatchNorm2d(128),
                     torch.nn.ReLU(),
 ##                    src.modules.PrototypeSimilarity(128, 128),
 ##                    src.modules.polynomial.Activation(128, n_degree=16),
