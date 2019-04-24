bac135dadbe2f41873f60b098b155a83739538c7
Yuchen Li <ychnlgy@gmail.com>
2019-04-24 14:15:28 -0400

    Added code

--- src/src/modules/ResBlock.py
+++ src/src/modules/ResBlock.py
@@ -4,12 +4,11 @@ IDENTITY = torch.nn.Sequential()
 
 class ResBlock(torch.nn.Module):
 
-    def __init__(self, block, shortcut=IDENTITY, activation=IDENTITY, allow_reverse=False):
+    def __init__(self, block, shortcut=IDENTITY, activation=IDENTITY):
         super(ResBlock, self).__init__()
         self.bk = block
         self.sc = shortcut
         self.ac = activation
-        self.allow_reverse = allow_reverse
         
     def forward(self, X):
         return self.ac(self.bk(X) + self.sc(X))
