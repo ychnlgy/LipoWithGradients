ab9e926f08ddad7bff1a051bca1a66728faf70de
Yuchen Li <ychnlgy@gmail.com>
2019-04-26 15:00:49 -0400

    Added code

--- src/mnist-polynomial.py
+++ src/mnist-polynomial.py
@@ -62,8 +62,8 @@ class Random(torch.nn.Module):
 
 def create_midact(d):
     return torch.nn.Sequential(
-        src.modules.PrototypeSimilarity(d, 8),
-        torch.nn.Conv2d(8, d, 1),
+        src.modules.PrototypeSimilarity(d, 4),
+        torch.nn.Conv2d(4, d, 1),
         torch.nn.BatchNorm2d(d),
         torch.nn.ReLU()
     )
