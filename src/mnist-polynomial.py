be20fe55a6f5c54d06b4530dd0f6234862034145
Yuchen Li <ychnlgy@gmail.com>
2019-04-26 15:32:37 -0400

    Added code

--- src/mnist-polynomial.py
+++ src/mnist-polynomial.py
@@ -62,8 +62,8 @@ class Random(torch.nn.Module):
 
 def create_midact(d):
     return torch.nn.Sequential(
-        torch.nn.Tanh(),
-        src.modules.polynomial.Activation(d, n_degree=4),
+        #torch.nn.Tanh(),
+        src.modules.polynomial.Activation(d, n_degree=4, a=-10, b=10),
         #src.modules.PrototypeSimilarity(d, 32),
         #torch.nn.Conv2d(32, d, 1),
         #torch.nn.BatchNorm2d(d),
