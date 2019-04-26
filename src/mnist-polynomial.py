ac61beffa4cdcf70ea2207bab1041663cec7250a
Yuchen Li <ychnlgy@gmail.com>
2019-04-26 14:28:25 -0400

    Added code

--- src/mnist-polynomial.py
+++ src/mnist-polynomial.py
@@ -89,7 +89,7 @@ def create_skipblock(d):
 
 def create_baseline_model(D, C):
     
-    d = 32
+    d = 64
 
     sim = src.modules.PrototypeSimilarity(d*4, 8)
     act = src.modules.polynomial.Activation(8, n_degree=32)
